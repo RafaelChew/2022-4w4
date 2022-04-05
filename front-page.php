80192b551f7135e7809e6719cd23abbf6057c3c3
RafaelChew <rafaelchew@hotmail.com>
2022-04-05 10:10:36 -0400

    premier commit ex03

---
+++ front-page.php
@@ -0,0 +1,26 @@
+<?php get_header() ?>
+<main class="site__main">
+    <section class="site__main-accueil">
+            <h1>Bienvenue au</h1>
+
+            <section class="animation">
+                <div class="animation__bloc">T</div>
+                <div class="animation__bloc">I</div>
+                <div class="animation__bloc">M</div>
+            </section>  
+
+            <h2>Technique d'intégration multimédia</h2>  
+             
+            <?php    
+                wp_nav_menu(array(
+                                "menu"=>"menu_accueil",
+                                "container"=>"nav",
+                                "container_class"=>"accueil__menu__nav",
+                                "menu_class"=>"accueil__menu__nav__ul",)); ?>
+                                
+            <div>
+
+            </div>
+    </section>   
+</main>
+<?php get_footer() ?>
